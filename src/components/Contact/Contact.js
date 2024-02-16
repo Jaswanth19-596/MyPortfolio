191af7d7394ef54d216d1d6e97cbb4ad5c8fb367
Jaswanth <madhajaswanth@gmail.com>
2024-02-15 23:17:55 -0600

    Changed Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,42 +1,44 @@
-import styles from "./Contact.module.css";
-import emailjs from "emailjs-com";
-import React, { useState, useRef } from "react";
-import LoadingSpinner from "./../LoadingSpinner/LoadingSpinner";
-import ContactMeImage from './../../assests/images/contact-me-final.png'
-import { FcApproval, FcCancel } from "react-icons/fc";
-import {Link} from 'react-router-dom';
-import { MdEmail } from "react-icons/md";
-import { FaDiscord } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaWhatsapp } from "react-icons/fa";
+import styles from './Contact.module.css';
+import emailjs from 'emailjs-com';
+import React, { useState, useRef, useEffect } from 'react';
+import LoadingSpinner from './../LoadingSpinner/LoadingSpinner';
+import ContactMeImage from './../../assests/images/contact-me-final.png';
+import { FcApproval, FcCancel } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
+import { MdEmail } from 'react-icons/md';
+import { FaDiscord } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaWhatsapp } from 'react-icons/fa';
 import './Contact2.css';
-
 
 const socialMediaHandles = [
   {
-    icon : <MdEmail className="icon"/>,
-    url : ''
+    icon: <MdEmail className="icon" />,
+    text: `Email me at : madhajaswanth@gmail.com or Click`,
+    url: '',
   },
   {
-    icon : <FaDiscord className="icon"/>,
-    url : ''
+    icon: <FaDiscord className="icon" />,
+    text: `Discord me at : madhajaswanth@gmail.com or Click`,
+    url: '',
   },
   {
-    icon : <FaInstagram className="icon"/>,
-    url : ''
+    icon: <FaInstagram className="icon" />,
+    text: 'DM me at : Jaswanth__m or Click',
+    url: '',
   },
   {
-    icon : <FaLinkedin className="icon"/>,
-    url : ''
+    icon: <FaLinkedin className="icon" />,
+    text: `Ping me at : madhajaswanth@gmail.com or Click`,
+    url: '',
   },
   {
-    icon : <FaDiscord className="icon"/>,
-    url : ''
+    icon: <FaWhatsapp className="icon" />,
+    text: `Text me at : +1 219 466 5564 or Click`,
+    url: '',
   },
-]
-
-
+];
 
 const Contact = () => {
   const form = useRef();
@@ -54,16 +56,28 @@ const Contact = () => {
 
   const [isFormValid, setIsFormValid] = useState(false);
 
+  const [active, setActive] = useState(0);
+
+  const changeActive = () => {
+    setActive((active + 1) % 5);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      changeActive();
+    }, 1000);
+  }, [active]);
+
   function sendEmail(e) {
     e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
     setIsLoading(true);
     console.log(e);
     emailjs
       .sendForm(
-        "service_zfvriac",
-        "template_d9sebc8",
+        'service_zfvriac',
+        'template_d9sebc8',
         form.current,
-        "RFMf-l38qADeDnHLi"
+        'RFMf-l38qADeDnHLi'
       )
       .then(
         (result) => {
@@ -91,7 +105,7 @@ const Contact = () => {
   };
 
   const emailBlurHandler = (e) => {
-    if (e.target.value.length >= 12 && e.target.value.endsWith("@gmail.com")) {
+    if (e.target.value.length >= 12 && e.target.value.endsWith('@gmail.com')) {
       setisEmailValid(true);
     } else {
       setisEmailValid(false);
@@ -104,7 +118,7 @@ const Contact = () => {
   };
 
   const messageBlurHandler = (e) => {
-    if (e.target.value.split(" ").length >= 2) {
+    if (e.target.value.split(' ').length >= 2) {
       setisMessageValid(true);
     } else {
       setisMessageValid(false);
@@ -120,57 +134,62 @@ const Contact = () => {
     <div className="container">
       <div className="top-container">
         <div className="contact-me-text-container">
-
-          <h3>Let's Talk</h3>
-          <p>If you have blocked me on all social media handles, You can contact me through the form below. And if you haven't blocked me, you can contact me through the social media handles.</p>
+          <h3 className="contact-me-heading">Let's Talk</h3>
+          <p className="contact-me-description">
+            If you have blocked me on all social media handles, You can contact
+            me through the form below. And if you haven't, you can contact me
+            with any one of the following one's.
+          </p>
 
           <div className="social-media-container">
-            {
-              socialMediaHandles.map(social => {
-              return <div className="social-media">
+            {socialMediaHandles.map((social, index) => {
+              return (
+                <div
+                  className={`social-media ${active === index ? 'active' : ''}`}
+                >
                   <div className="social-media-icon-container">
-                    {socialMediaHandles.icon}
+                    {social.icon}
                   </div>
-                  <div className="social-media-text-container">
-                    Email me at : madhajaswanth@gmail.com or Click : <Link>something</Link>
-
-                  </div>
+                  <p className="social-media-text">{social.text}</p>
                 </div>
-              })
-            }          
+              );
+            })}
           </div>
         </div>
 
-
         <div className="contact-me-image-container">
-          <img className='contact-me-image' src={ContactMeImage} alt='contact-me' />
+          <img
+            className="contact-me-image"
+            src={ContactMeImage}
+            alt="contact-me"
+          />
         </div>
       </div>
 
       {error && (
-        <div className={styles["new-container"]}>
-          <p className={styles["email-fail"]}>
+        <div className={styles['new-container']}>
+          <p className={styles['email-fail']}>
             Email cannot be sent due to some issue
             <FcCancel />
           </p>
-          <p className={styles["email-fail-sub"]}>
+          <p className={styles['email-fail-sub']}>
             You can contact me at madhajaswanth@gmail.com
           </p>
         </div>
       )}
 
       {!error && isLoading && (
-        <div className={styles["new-container"]}>
+        <div className={styles['new-container']}>
           {!emailSent && <LoadingSpinner />}
         </div>
       )}
 
       {!error && !isLoading && emailSent && (
-        <div className={styles["new-container"]}>
-          <p className={styles["email-success"]}>
+        <div className={styles['new-container']}>
+          <p className={styles['email-success']}>
             Email Received Successfully <FcApproval />
           </p>
-          <p className={styles["email-success-sub"]}>
+          <p className={styles['email-success-sub']}>
             I will try to get back to you as soon as possible
           </p>
         </div>
@@ -183,12 +202,12 @@ const Contact = () => {
           >
             Contact
           </h3>
-          <p className={`u-center-text ${styles["para"]}`}>
+          <p className={`u-center-text ${styles['para']}`}>
             Feel free to Contact me by submitting the form below and I will get
             back to you as soon as possible
           </p>
 
-          <div className={styles["input__group"]}>
+          <div className={styles['input__group']}>
             <label htmlFor="fullname" className={styles.label}>
               FullName
             </label>
@@ -205,7 +224,7 @@ const Contact = () => {
               <p className={styles.invalidText}>*invalid name</p>
             )}
           </div>
-          <div className={styles["input__group"]}>
+          <div className={styles['input__group']}>
             <label htmlFor="email" className={styles.label}>
               Email
             </label>
@@ -222,7 +241,7 @@ const Contact = () => {
               <p className={styles.invalidText}>*invalid email</p>
             )}
           </div>
-          <div className={styles["input__group"]}>
+          <div className={styles['input__group']}>
             <label htmlFor="fullname" className={`${styles.label}`}>
               Message
             </label>
