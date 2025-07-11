013e7a6c3847ebfc2b8e94285fda8154555cc015
Jaswanth <madhajaswanth@gmail.com>
2025-07-11 18:59:09 -0500

    Just testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./pages/RootLayout";
-import HomePage from "./pages/HomePage";
-import ProjectPage from "./pages/ProjectPage";
-import ContactPage from "./pages/ContactPage";
-import AboutPage from "./pages/AboutPage";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/RootLayout';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
+import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "projects", element: <ProjectPage /> },
-      { path: "contact", element: <ContactPage /> },
-      { path: "about", element: <AboutPage /> },
+      { path: 'projects', element: <ProjectPage /> },
+      { path: 'contact', element: <ContactPage /> },
+      { path: 'about', element: <AboutPage /> },
+      { path: 'Testing', element: <AboutPage /> },
     ],
   },
 ]);
